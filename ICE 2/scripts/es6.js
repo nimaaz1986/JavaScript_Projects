d6a51f683ec161b912bf50ad121c9da82cc07257
nimaaz1986 <105405845+nimaaz1986@users.noreply.github.com>
2023-01-27 14:36:14 -0500

    ICE2- comment

---
+++ ICE 2/scripts/es6.js
@@ -0,0 +1,35 @@
+console.log("es6.js loaded");
+
+// #region ES6 Arrow Functions
+// ES5
+function printNameES5(name){
+    return ('ES5 Traditional Function: Hello ' + name);
+}
+
+// ES6
+
+
+// We can also write the above code without the return statement
+// Called an implicit return
+
+
+
+
+
+//#endregion ES6 Arrow Functions
+
+//#region ES6 Function Parameters
+
+// ES6 default parameters
+
+
+// ES7 Default parameters in arrow functions (OOOOH!)
+
+// New 'rest' parameters
+
+
+//#endregion ES6 Function Parameters
+
+//#region Cleaner Code
+
+//#endregion Cleaner Code
---
+++ ICE 2/scripts/formValidation.js
@@ -0,0 +1,99 @@
+console.log("formValidation.js loaded");
+
+/**
+ * Validate the email address
+ * @param {string} email    the email address to validate
+ * @returns {boolean}        to indicate if the email is valid
+ */
+
+//TODO:
+// Make all fields required --Done
+// Add a pattern attribute in the telephone element (HTML) -- Done
+// Ensure the password is redacted into dots on screen when typed in - Done 
+// Create a function that knows if the username is valid (feel free to grab some RegEx from the Interwebs - cite source and test it!) - Done
+// If it is not valid, be sure to update the #generalError <p></p> with a good error message and some highlighting (red background, maybe?)
+// Validate the username when the form is submitted
+// Clear any additional error message and highlighting when the form is reset
+// COMMENT EVERYTHING :D
+// Pop your .js file into the meeting chat when you're done and we'll have a look at some examples! (Feel free to leave your name off)
+
+function validateUsername(usernameString) {
+    console.log('in ValidateUsername');
+    let genErEl = document.getElementById('generalError');
+    let usernameInputEl = document.getElementById('usernameInput');
+    let usernameErrorEl = document.getElementById('usernameError');
+    let backgroundColor = "#ffffff";
+    let genErMsg = "";
+    let usernameErMsg = "";
+
+    let usernameRegex = /^\S*$/;
+    if (usernameString.length < 5) {
+        // we have a specific error for general error massage . the length      of       message less that 5 
+        genErMsg = "Username must be at least 5 characters long";
+        usernameErMsg = "Username must be at least 5 characters long";
+        backgroundColor = "#e5989b";
+    } else if (usernameString.length > 20) {
+        genErMsg = "Username must be less than 20 characters long";
+        usernameErMsg = "Username must be less than 20 characters long";
+        backgroundColor = "#e5989b";
+    } else if (!usernameString.match(usernameRegex)) {
+        genErMsg = "Username cannot contain spaces";
+        usernameErMsg = "Username cannot contain spaces";
+        backgroundColor = "#e5989b";
+    }
+
+    genErEl.innerHTML = genErMsg;
+    usernameErrorEl.innerHTML = usernameErMsg;
+    usernameInputEl.style.backgroundColor = backgroundColor;
+}
+
+let submitButton = document.getElementById('submit-reg-form');
+if (submitButton) {
+    submitButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        let username = document.getElementById('usernameInput').value;
+        validateUsername(username)       
+    })
+};
+
+
+// function isValidUsername(username) {
+//     // Regular expression for validating the username
+//     // The pattern requires the string to start with a letter,
+//     // contain only letters, numbers, or underscores,
+//     // and have a length between 3 and 15 characters
+//     // (source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript)
+    
+//     var pattern = /^[a-zA-Z][a-zA-Z0-9_]{2,14}$/;
+//     return username.match(pattern);
+//   }
+let submitRest = document.getElementById('submit-reg-form1');
+
+submitRest.addEventListener('click', function(e) {
+    //e.preventDefault();
+    resetForm();
+
+})
+   
+
+// Rest function for all error massages backgroud colors
+function resetForm() {
+
+    // genErEl.innerHTML = '';
+    // usernameErrorEl.innerHTML = '';
+    let usernameInputEl = document.getElementById('usernameInput');
+    let usernameErrorEl = document.getElementById('usernameError');
+    usernameInputEl.style.backgroundColor = "#ffffff";
+    // genErEl = 
+    generalError.innerHTML = '';
+    usernameErrorEl.innerHTML = '';
+    backgroundColor = "#ffffff";
+
+    
+
+  
+
+  }
+
+
+
