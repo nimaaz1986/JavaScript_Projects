aafdc30e758d3ce749d90bf58b0aec65e015fd02
nimaaz1986 <105405845+nimaaz1986@users.noreply.github.com>
2023-02-10 22:00:20 -0500

    Commit ICE 4

---
+++ ICE 4/scripts/form.js
@@ -0,0 +1,74 @@
+/**
+ * validateFirst - validated user first name entry through registration form
+ * @param {string} firstName
+ * @returns html element for error message
+ */
+export function validateFirst(firstName) {
+    if (firstName.length < 2) {
+        return "<p>You have entered a first name that is too short.</p>"
+    }
+    else {
+        return "<p></p>";
+    }
+};
+
+/**
+ * validateLast- validated user last name entry through registration form
+ * @param {string} last
+ * @returns html element for error message
+ */
+export function validateLast(last) {
+    if (last.length < 2) {
+        return "<p>You have entered a last name that is too short.</p>"
+    }
+    else {
+        return "<p></p>";
+    }
+};
+
+
+/**
+ * validateUsername- validated user username entry through registration form
+ * @param {string} username
+ * @returns html element for error message
+ */
+export function validateUsername(username) {
+if (username.length < 2) {
+        return "<p>You have entered a username that is too short.</p>"
+    }
+    else {
+        return "<p></p>";
+    }
+};
+
+/**
+ * validateEmail- validated user email entry through registration form
+ * @param {string} email
+ * @returns html element for error message
+ */
+export function validateEmail(email) {
+if (email.length < 5) {
+        return "<p>You have entered am email that is too short.</p>"
+    }
+    else {
+        return "<p></p>";
+    }
+};
+
+/**
+ * validatePassword- validated user password entries 1 and 2 are the same through ristration form
+ * @param {string} password1
+ * @param {string} password2
+ * @returns html element for error message
+ */
+export function validatePassword(pass1, pass2) {
+
+    if (pass1.length > 0 & pass1 == pass2) {
+        return "<p></p>";
+    } else {
+        return "<p>Your passwords do not match.</p>"
+    }
+
+};
+
+
